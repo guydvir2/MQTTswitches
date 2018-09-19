d424345116f3db1aa2c2feaff33b879d77b36a01
Guy Dvir <guydvir2@gmail.com>
2018-09-19 21:13:24 +0300

    update MQQT to include on-off schedule

--- start_server.py
+++ start_server.py
@@ -9,14 +9,26 @@ def start_logger(topics, server, user, password):
     logger.start()
 
 
-def start_scheduler(server, user, password, msg_topic, pub_topic):
+def start_windows_scheduler(server, user, password, msg_topic, pub_topic):
     topic_prefix = 'HomePi/Dvir/Windows/'
     Home_Devices = ['pRoomWindow', 'fRoomWindow', 'kRoomWindow']
     Home_Devices = [topic_prefix + device for device in Home_Devices]
     for client in Home_Devices:
         Sched_MQTT.MQTTRemoteSchedule(broker=server, device_topic=client, scheds_topic=pub_topic,
-                                      msg_topic=msg_topic, username=user, password=password,
-                                      sched_filename='/home/guy/github/SmartHome_SoC/MQTTswitches/' + client.split('/')[-1] + '.json')
+                                      msg_topic=msg_topic, username=user, password=password, device_type="window",
+                                      sched_filename='/home/guy/github/SmartHome_SoC/MQTTswitches/' + client.split('/')[
+                                          -1] + '.json')
+
+
+def start_lights_scheduler(server, user, password, msg_topic, pub_topic):
+    topic_prefix = 'HomePi/Dvir/Switches/'
+    Home_Devices = ['S1', 'S2']
+    Home_Devices = [topic_prefix + device for device in Home_Devices]
+    for client in Home_Devices:
+        Sched_MQTT.MQTTRemoteSchedule(broker=server, device_topic=client, scheds_topic=pub_topic,
+                                      msg_topic=msg_topic, username=user, password=password, device_type="on_off",
+                                      sched_filename='/home/guy/github/SmartHome_SoC/MQTTswitches/' + client.split('/')[
+                                          -1] + '.json')
 
 
 # ######################### Parameters ###########################
@@ -32,4 +44,5 @@ PASSWORD = "kupelu9e"
 # ######### start here ############
 start_logger(topics=TOPICS2LOG, server=BROKER, user=USER, password=PASSWORD)
 time.sleep(5)
-start_scheduler(server=BROKER, user=USER, password=PASSWORD, msg_topic=MSG_TOPIC, pub_topic=ADDITIONAL_TOPIC)
+start_windows_scheduler(server=BROKER, user=USER, password=PASSWORD, msg_topic=MSG_TOPIC, pub_topic=ADDITIONAL_TOPIC)
+start_lights_scheduler(server=BROKER, user=USER, password=PASSWORD, msg_topic=MSG_TOPIC, pub_topic=ADDITIONAL_TOPIC)
